a695c8299af20bc73b11bd92c86fbd439b792c79
DESKTOP-TMIDRT3\Junior Felix <juniorfelixgb@gmail.com>
2020-09-24 14:46:04 -0400

    add Countries CRUD

---
+++ OneSales/OneSales.Web/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesOne.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneSales.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base (options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>()
+                        .HasIndex(c => c.Name)
+                        .IsUnique();
+        }
+    }
+}
---
+++ OneSales/SalesOne.Common/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SalesOne.Common.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
